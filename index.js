d7bf13b90bf60bd6ebaae640637ae899b210b5f3
Rehnell Blackett <149915517+Rehnell@users.noreply.github.com>
2025-03-29 12:47:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-$("#search-icon").click(function() {
-    $(".nav").toggleClass("search");
-    $(".nav").toggleClass("no-search");
-    $(".search-input").toggleClass("search-active");
-  });
-  
-  $('.menu-toggle').click(function(){
-     $(".nav").toggleClass("mobile-nav");
-     $(this).toggleClass("is-active");
-  });
-
 document.addEventListener("DOMContentLoaded", function () {
     const menuBtn = document.getElementById("menu-btn");
     const body = document.body;
