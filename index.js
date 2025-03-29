f2667c3ea5bd5335890abad9e1ac5b15db42af29
Rehnell Blackett <149915517+Rehnell@users.noreply.github.com>
2025-03-29 11:32:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ $("#search-icon").click(function() {
      $(".nav").toggleClass("mobile-nav");
      $(this).toggleClass("is-active");
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("menu-btn");
+    const body = document.body;
+
+    menuBtn.addEventListener("change", function () {
+        if (this.checked) {
+            body.style.backgroundColor = "white"; // Change background to white
+        } else {
+            body.style.backgroundColor = "rgb(37, 172, 185)"; // Revert to original color
+        }
+    });
+});
